fefe838895c634c48c1ebaad2dcba16b52fca100
jazz287 <wadhwa.jasmine28@gmail.com>
2024-06-21 20:39:41 +0530

    main_input

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import Header from "./components/header"
+import MainInput from "./components/main_input"
+import "./styles/App.css"
+
 function App() {
   return (
     <>
+      <Header />
+      <MainInput/>
     </>
   )
 }
---
+++ src/components/main_input.jsx
@@ -0,0 +1,32 @@
+import "../styles/main_input.css";
+import { useState } from "react";
+import { PlusOutlined } from "@ant-design/icons";
+
+function MainInput() {
+  const [focused, setFocused] = useState(false);
+  const onFocus = () => setFocused(true);
+  const onBlur = () => setFocused(false);
+
+  return (
+    <div className={focused ? "input-wrapper" : ""}>
+      <div className="main-input">
+        <div className="prefix">
+          <PlusOutlined
+            style={{
+              width: "15px",
+              height: "15px",
+            }}
+          />
+        </div>
+        <input
+          type="text"
+          placeholder="Search for or add a Task"
+          onFocus={onFocus}
+          onBlur={onBlur}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default MainInput;
